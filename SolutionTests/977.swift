6934ce4b45c6a8092dc9a127028115168713b8e6
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-16 14:11:57 +0900

    테스트케이스 별로 테스트 함수 구분해서 재작성

--- SolutionTests/977.swift
+++ SolutionTests/977.swift
@@ -34,21 +34,32 @@ func sortedSquares(_ A: [Int]) -> [Int] {
 // Test Code
 extension SolutionTests {
     
-    func testSortedSquares() {
+    func testSortedSquares_case1() {
         // Given-When-Then (준비-실행-검증)
         
         // Given
-        let inputValue = [[-4,-1,0,3,10], [-7,-3,2,3,11]]
-        let outputValue = [[0,1,9,16,100], [4,9,9,49,121]]
+        let input = [-4,-1,0,3,10]
+        let output = [0,1,9,16,100]
         
         // When
-        var testedValue = outputValue
-        testedValue[0] = sortedSquares(inputValue[0])
-        testedValue[1] = sortedSquares(inputValue[1])
+        let result = sortedSquares(input)
+      
+        // Then
+        XCTAssertEqual(result, output)
+    }
+    
+    func testSortedSquares_case2() {
+        // Given-When-Then (준비-실행-검증)
+        
+        // Given
+        let input = [-7,-3,2,3,11]
+        let output = [4,9,9,49,121]
+          
+        // When
+        let result = sortedSquares(input)
         
         // Then
-        XCTAssertEqual(testedValue[0], outputValue[0])
-        XCTAssertEqual(testedValue[1], outputValue[1])
+        XCTAssertEqual(result, output)
     }
     
 }

5bd34d90038d14dc0d9ee79a91ede627f1c06fdf
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-21 19:29:14 +0900

    #1299 솔루션, 테스트 코드 추가

--- SolutionTests/1299.swift
+++ SolutionTests/1299.swift
@@ -3,3 +3,72 @@
 //
 
 import Foundation
+import XCTest
+
+/**
+ #1299. Replace Elements with Greatest Element on Right Side
+ 
+ Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+ After doing so, return the array.
+ 
+ Example 1:
+ ~~~~
+ Input: arr = [17,18,5,4,6,1]
+ Output: [18,6,6,6,1,-1]
+ ~~~~
+ 
+ Constraints:
+    - 1 <= arr.length <= 10^4
+    - 1 <= arr[i] <= 10^5
+ 
+ Submissions:
+    - Runtime: 172 ms, faster than 50.51% of Swift online submissions for Replace Elements with Greatest Element on Right Side.
+    - Memory Usage: 21.5 MB, less than 100.00% of Swift online submissions for Replace Elements with Greatest Element on Right Side.
+*/
+func replaceElements(_ arr: [Int]) -> [Int] {
+    guard !arr.isEmpty else {
+        return []
+    }
+    
+    if arr.count == 1 {
+        return [-1]
+    }
+    
+    var result = arr
+    result.append(-1)
+    result.reverse()
+    result.removeLast()
+    
+    let length = arr.count - 1
+    var max = -2
+    
+    for i in 0...length {
+        max = max > result[i] ? max:result[i]
+        result[i] = max
+    }
+    
+             
+    
+    return result.reversed()
+}
+
+// Test Code
+extension SolutionTests {
+    
+    func testReplaceElements() {
+        // Given-When-Then (준비-실행-검증)
+        
+        // Given
+        let input = [17,18,5,4,6,1]
+        let output = [18,6,6,6,1,-1]
+        
+        // When
+        let result = replaceElements(input)
+        
+        // Then
+        XCTAssertEqual(result, output)
+    }
+    
+    
+}

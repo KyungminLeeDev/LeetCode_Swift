09e830d0915e4984e1ddb8fe707ae68d6bab5f25
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-21 22:29:11 +0900

    #1299 주석에 Solution 추가

--- SolutionTests/1299.swift
+++ SolutionTests/1299.swift
@@ -8,10 +8,15 @@ import XCTest
 /**
  #1299. Replace Elements with Greatest Element on Right Side
  
- Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
-
- After doing so, return the array.
+ - Difficulty: Easy
+ - Topics: Array
  
+ Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+ After doing so, return the array.
+
+ 배열 'arr'이 주어지면, 해당 배열의 모든 원소를 오른쪽의 원소 중 가장 큰 원소로 바꾸고, 마지막 원소를 '-1'로 바꿉니다.
+ 그런 다음 배열을 반환하세요.
+
  Example 1:
  ~~~~
  Input: arr = [17,18,5,4,6,1]
@@ -19,12 +24,15 @@ import XCTest
  ~~~~
  
  Constraints:
-    - 1 <= arr.length <= 10^4
-    - 1 <= arr[i] <= 10^5
+ - 1 <= arr.length <= 10^4
+ - 1 <= arr[i] <= 10^5
  
  Submissions:
-    Runtime: 136 ms, faster than 96.94% of Swift online submissions for Replace Elements with Greatest Element on Right Side.
-    Memory Usage: 21.8 MB, less than 70.00% of Swift online submissions for Replace Elements with Greatest Element on Right Side.
+ - Runtime: 136 ms, faster than 96.94% of Swift online submissions for Replace Elements with Greatest Element on Right Side.
+ - Memory Usage: 21.8 MB, less than 70.00% of Swift online submissions for Replace Elements with Greatest Element on Right Side.
+ 
+ Solution:
+ - 배열의 마지막 원소의 직전 원소부터 역순으로 순회하며 해당 원소의 이전 원소까지의 최대값을 갱신해가며 현재 원소에 저장한다.
 */
 func replaceElements(_ arr: [Int]) -> [Int] {
     guard !arr.isEmpty else {

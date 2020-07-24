6c93f83905552ed46b06362ee00c94f746edcab3
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-24 15:04:57 +0900

    #1051 주석 일부 들여쓰기 수정

--- SolutionTests/1051.swift
+++ SolutionTests/1051.swift
@@ -24,11 +24,11 @@ import XCTest
  Input: heights = [1,1,4,2,1,3]
  Output: 3
  Explanation:
- Current array : [1,1,4,2,1,3]
- Target array  : [1,1,1,2,3,4]
- On index 2 (0-based) we have 4 vs 1 so we have to move this student.
- On index 4 (0-based) we have 1 vs 3 so we have to move this student.
- On index 5 (0-based) we have 3 vs 4 so we have to move this student.
+    Current array : [1,1,4,2,1,3]
+    Target array  : [1,1,1,2,3,4]
+    On index 2 (0-based) we have 4 vs 1 so we have to move this student.
+    On index 4 (0-based) we have 1 vs 3 so we have to move this student.
+    On index 5 (0-based) we have 3 vs 4 so we have to move this student.
  ~~~~
  Example 2:
  ~~~~

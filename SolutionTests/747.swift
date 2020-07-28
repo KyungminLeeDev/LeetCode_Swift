3f37d586cb40a4b3c8e5c80c3967d285cbfb7afc
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-28 14:10:31 +0900

    #747 솔루션, 테스트 코드 추가

--- SolutionTests/747.swift
+++ SolutionTests/747.swift
@@ -4,3 +4,57 @@
 
 import Foundation
 import XCTest
+
+func dominantIndex(_ nums: [Int]) -> Int {
+    guard nums.count != 1 else {
+        return 0
+    }
+    
+    let max = nums.max()!
+    
+    for n in nums {
+        if n == max {
+            continue
+        }
+        
+        if n * 2 > max {
+            return -1
+        }
+    }
+    
+    return nums.firstIndex(of: max)!
+}
+
+// Test Code
+extension SolutionTests {
+    
+    func testDominantIndex_case1() {
+        // Given-When-Then (준비-실행-검증)
+        
+        // Given
+        let input = [3,6,1,0]
+        let output = 1
+        
+        // When
+        let result = dominantIndex(input)
+        
+        // Then
+        XCTAssertEqual(result, output)
+    }
+    
+    func testDominantIndex_case2() {
+        // Given-When-Then (준비-실행-검증)
+        
+        // Given
+        let input = [1,2,3,4]
+        let output = -1
+        
+        // When
+        let result = dominantIndex(input)
+        
+        // Then
+        XCTAssertEqual(result, output)
+    }
+    
+}
+

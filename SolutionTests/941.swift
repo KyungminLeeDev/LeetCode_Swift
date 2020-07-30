054f16e3f7af6862550a150ff6479c96361fbf8f
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-30 21:10:41 +0900

    #941 주석에 해석 추가

--- SolutionTests/941.swift
+++ SolutionTests/941.swift
@@ -11,34 +11,45 @@ import XCTest
  Given an array A of integers, return true if and only if it is a valid mountain array.
 
  Recall that A is a mountain array if and only if:
-    - A.length >= 3
-    - There exists some i with 0 < i < A.length - 1 such that:
-        - A[0] < A[1] < ... A[i-1] < A[i]
-        - A[i] > A[i+1] > ... > A[A.length - 1]
+ - A.length >= 3
+ - There exists some i with 0 < i < A.length - 1 such that:
+    - A[0] < A[1] < ... A[i-1] < A[i]
+    - A[i] > A[i+1] > ... > A[A.length - 1]
+ 
+ 
+ 정수 배열 A가 주어지면, 이 배열이 유효한 '산 배열' 인 경우에만 'true'를 리턴하세요.
+
+ 다음과 같은 경우에만 A가 '산 배열'임을 기억하세요:
+ - A.length >= 3
+ - There exists some i with 0 < i < A.length - 1 such that:
+     - A[0] < A[1] < ... A[i-1] < A[i]
+     - A[i] > A[i+1] > ... > A[A.length - 1]
  
  Example 1:
  ~~~~
  Input: [2,1]
  Output: false
  ~~~~
+ 
  Example 2:
  ~~~~
  Input: [3,5,5]
  Output: false
  ~~~~
+ 
  Example 3:
  ~~~~
  Input: [0,3,2,1]
  Output: true
  ~~~~
  
- - note:
-    - 0 <= A.length <= 10000
-    - 0 <= A[i] <= 10000
+ Note:
+ - 0 <= A.length <= 10000
+ - 0 <= A[i] <= 10000
  
  Submissions:
-    - Runtime: 268 ms, faster than 44.75% of Swift online submissions for Valid Mountain Array.
-    - Memory Usage: 20.9 MB, less than 84.09% of Swift online submissions for Valid Mountain Array.
+ - Runtime: 268 ms, faster than 44.75% of Swift online submissions for Valid Mountain Array.
+ - Memory Usage: 20.9 MB, less than 84.09% of Swift online submissions for Valid Mountain Array.
 */
 func validMountainArray(_ A: [Int]) -> Bool {
     guard A.count > 2 else {

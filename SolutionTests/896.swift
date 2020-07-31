5e35ace7ce447b8568947f5c82fe2ed7dd84bd22
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-31 14:43:55 +0900

    #896 솔루션 추가

--- SolutionTests/896.swift
+++ SolutionTests/896.swift
@@ -4,3 +4,23 @@
 
 import Foundation
 import XCTest
+
+func isMonotonic(_ A: [Int]) -> Bool {
+    var up = true
+    var down = true
+    
+    for i in 0..<(A.count - 1) {
+        if up && A[i] > A[i + 1] {
+            up = false
+        } else if down && A[i] < A[i + 1] {
+            down = false
+        }
+        
+        // 둘다 false면 바로 false 리턴
+        if !up && !down {
+            return false
+        }
+    }
+    
+    return true
+}

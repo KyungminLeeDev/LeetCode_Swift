0907a624d90b5750d75668ac7841d12c2263d726
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-08-01 23:28:52 +0900

    #217 주석 추가

--- SolutionTests/217.swift
+++ SolutionTests/217.swift
@@ -5,6 +5,46 @@
 import Foundation
 import XCTest
 
+/**
+ #217. Contains Duplicate
+ 
+ - Difficulty: Easy
+ - Topics: Array
+ 
+ Given an array of integers, find if the array contains any duplicates.
+
+ Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+ 
+ 정수 배열이 주어지면 배열에 중복 항목이 있는지 찾으세요.
+
+ 당신의 함수는 배열에 값이 두 번 이상 나타나면 true를 반환하고 모든 원소가 고유하면 false를 반환해야합니다.
+
+ Example 1:
+ ~~~~
+ Input: [1,2,3,1]
+ Output: true
+ ~~~~
+ 
+ Example 2:
+ ~~~~
+ Input: [1,2,3,4]
+ Output: false
+ ~~~~
+ 
+ Example 3:
+ ~~~~
+ Input: [1,1,1,3,3,4,3,2,4,2]
+ Output: true
+ ~~~~
+ 
+ Solution:
+ - 중복된 원소가 없는 Set의 특성을 활용한다.
+ - 배열을 Set으로 형변환해서 배열과 Set의 원소 갯수를 비교해서 같으면 중복된 원소가 없다고 판단한다.
+ 
+ Submissions:
+ - Runtime: 572 ms, faster than 42.64% of Swift online submissions for Find All Numbers Disappeared in an Array.
+ - Memory Usage: 23.7 MB, less than 57.14% of Swift online submissions for Find All Numbers Disappeared in an Array.
+*/
 func containsDuplicate(_ nums: [Int]) -> Bool {
     let numsSet = Set(nums)
             
